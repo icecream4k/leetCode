490347f0b31dfb6449c7f64c80e4816f077ed0e0
icecream4k <969666859@qq.com>
2021-04-28 22:22:02 +0800

    04-28

--- test.js
+++ test.js
@@ -1,12 +1,4 @@
-const obj1 = {
-    color: 'red',
-    widht: 200,
-    height: 400,
-}
-const a = obj1 => obj1
-
-const b = obj1 => obj1
-a.color = 'green'
-b.color = 'yellow'
-console.log(`a => ${a.color}`);
-console.log(`b => ${b.color}`);
+var array = ["one", "two", "four"];
+console.log(array)
+array.splice(2, 0, "three");
+console.log(array)
