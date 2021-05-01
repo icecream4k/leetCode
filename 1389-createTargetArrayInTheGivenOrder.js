9792b426748dc9ce4ea69a1a7a726bb2b5d2f600
icecream4k <969666859@qq.com>
2021-05-01 18:23:20 +0800

    05-01

--- 1389-createTargetArrayInTheGivenOrder.js
+++ 1389-createTargetArrayInTheGivenOrder.js
@@ -16,6 +16,7 @@
 // 2            2        [0,1,2]
 // 3            2        [0,1,3,2]
 // 4            1        [0,4,1,3,2]
+
 /**
  * @param {number[]} nums
  * @param {number[]} index
--- test.js
+++ test.js
@@ -1,4 +1,18 @@
-var array = ["one", "two", "four"];
-console.log(array)
-array.splice(2, 0, "three");
-console.log(array)
+function mondayArray(dateString) {
+    let dateStringReg = /^\d{4}[/-]\d{1,2}[/-]\d{1,2}$/
+
+    if (dateString.match(dateStringReg)) {
+        let presentDate = new Date(dateString),
+            today = presentDate.getDay() !== 0 ? presentDate.getDay() : 8
+
+        return Array.from(new Array(8), function(val, index) {
+            return formatDate(new Date(presentDate.getTime() - (today - index - 1) * 24 * 60 * 60 * 1000))
+        })
+    }
+
+    function formatDate(date) {
+        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    }
+}
+let weekArray = mondayArray('2021-04-28')
+console.log(weekArray);
