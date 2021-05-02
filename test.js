96b628b2c35b005aa3494b06274db60320b5dc2f
icecream4k <969666859@qq.com>
2021-05-02 10:01:49 +0800

    05-02

--- test.js
+++ test.js
@@ -1,18 +1,7 @@
-function mondayArray(dateString) {
-    let dateStringReg = /^\d{4}[/-]\d{1,2}[/-]\d{1,2}$/
-
-    if (dateString.match(dateStringReg)) {
-        let presentDate = new Date(dateString),
-            today = presentDate.getDay() !== 0 ? presentDate.getDay() : 8
-
-        return Array.from(new Array(8), function(val, index) {
-            return formatDate(new Date(presentDate.getTime() - (today - index - 1) * 24 * 60 * 60 * 1000))
-        })
-    }
-
-    function formatDate(date) {
-        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+number = 12345
+for (let i = 0; i < number; i++) {
+    console.log(i);
+    if(i == number){
+        console.log('算对了');
     }
 }
-let weekArray = mondayArray('2021-04-28')
-console.log(weekArray);
