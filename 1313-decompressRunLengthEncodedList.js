d3042afce3c6562a19221ab53bbd87224ee6fc85
ICEcream4K <969666859@qq.com>
2021-05-05 16:29:47 +0800

    05-05

---
+++ 1313-decompressRunLengthEncodedList.js
@@ -0,0 +1,34 @@
+// 给你一个以行程长度编码压缩的整数列表 nums 。
+
+// 考虑每对相邻的两个元素 [freq, val] = [nums[2*i], nums[2*i+1]] （其中 i >= 0 ），每一对都表示解压后子列表中有 freq 个值为 val 的元素，你需要从左到右连接所有子列表以生成解压后的列表。
+
+// 请你返回解压后的列表。
+
+// 输入：nums = [1,2,3,4]
+// 输出：[2,4,4,4]
+// 解释：第一对 [1,2] 代表着 2 的出现频次为 1，所以生成数组 [2]。
+// 第二对 [3,4] 代表着 4 的出现频次为 3，所以生成数组 [4,4,4]。
+// 最后将它们串联到一起 [2] + [4,4,4] = [2,4,4,4]。
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var decompressRLElist = function(nums) {
+    let tempStartNumber = null;
+    let tempEndNumber = null;
+    let tempArray = new Array;
+    for (let i = 1; i < nums.length; i++) {
+        if (i % 2 !== 0) {
+            tempStartNumber = nums[i]
+        } else {
+            tempEndNumber = nums[i]
+            for (let n = 0; n < tempStartNumber; n++) {
+                tempArray.push(tempEndNumber)
+                console.log(`正在执行中 - ${tempStartNumber}  -- ${tempEndNumber}`);
+            }
+        }
+    }
+    console.log(tempArray);
+};
+decompressRLElist([1, 2, 3, 4])
