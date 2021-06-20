fe7462b463b1fa2008d5c3c3542b3298363af628
ICEcream4K <969666859@qq.com>
2021-06-20 23:33:08 +0800

    06-20

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log(object);
+// function sleep(numberMillis) {
+//     var now = new Date();
+//     var exitTime = now.getTime() + numberMillis;
+//     while (true) {
+//         now = new Date();
+//         if (now.getTime() > exitTime)
+//             return;
+//     }
+// }
+// console.log('哈哈');
+// sleep(5000)
+// console.log('嘿嘿');
+
+
+
+console.log(s3)
