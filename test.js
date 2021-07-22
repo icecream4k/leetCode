2eda52ab501d6ee0473e459fd70818829cf763ae
Lawrence Han <969666859@qq.com>
2021-07-22 09:47:12 +0800

    07-22

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
-let [a,b,c,d,e] = [null,null,null,null,null]
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
+const planningCar = async () => {
+    return await this.$dataLNG.get('/bigScreenShow/todayPlans', {
+        headers: {
+            "x-auth-token": res.data.data['token']
+        }
+    })
+};
+const plannedWeight = async () => {
+    return await this.$dataLNG.get('/bigScreenShow/todayPlansWeight', {
+        headers: {
+            "x-auth-token": res.data.data['token']
+        }
+    })
+};
+(async () => {
+    const [planningCarFun, plannedWeightFun] = await Promise.all([
+        planningCar(),
+        plannedWeight()
+    ]);
+    console.log('promise.all方法计划重量:', plannedWeightFun.data.data);
+    console.log('promise.all方法计划车辆', planningCarFun.data.data);
+})()
