7031b66a7c35c72c02f38d6075d07fd061f44783
Lawrence Han <969666859@qq.com>
2021-08-06 17:14:46 +0800

    08-06

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-let a = [1,2,3]
-let b = [1,2]
-console.log(a.filter(key => !b.includes(key)));
+// let s = '11 22 33 44 55';
+// console.log(s.includes('3'));
+let s1 = '11';
+let s2 = '1';
+console.log(s1.padStart(2, '0'));
+
+console.log(s2.padStart(2, '0'));
